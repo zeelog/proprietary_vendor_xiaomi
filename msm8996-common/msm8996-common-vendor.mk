70b93eaa79754a6626ee50aa86d52be7756f2924
Luca Stefani <luca020400@lineageos.org>
2017-08-25 00:10:17 +0200

    msm8996-common: Hax libsettings to load N libprotobuf
    sed -i "s/libprotobuf-cpp-full.so/libprotobuf-cpp-N.so\x00\x00\x00/" vendor/lib64/libsettings.so
    sed -i "s/libprotobuf-cpp-full.so/libprotobuf-cpp-N.so\x00\x00\x00/" vendor/lib64libprotobuf-cpp-full.so
     -> mv vendor/lib64libprotobuf-cpp-full.so vendor/lib64libprotobuf-cpp-N.so
    
    Change-Id: Id76f93c8320f6b6c9b18fc0306415ce1d1a38457

--- msm8996-common/msm8996-common-vendor.mk
+++ msm8996-common/msm8996-common-vendor.mk
@@ -509,6 +509,7 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/msm8996-common/proprietary/vendor/lib64/libmmosal.so:system/vendor/lib64/libmmosal.so \
     vendor/xiaomi/msm8996-common/proprietary/vendor/lib64/libnetmgr.so:system/vendor/lib64/libnetmgr.so \
     vendor/xiaomi/msm8996-common/proprietary/vendor/lib64/libperipheral_client.so:system/vendor/lib64/libperipheral_client.so \
+    vendor/xiaomi/msm8996-common/proprietary/vendor/lib64/libprotobuf-cpp-N.so:system/vendor/lib64/libprotobuf-cpp-N.so \
     vendor/xiaomi/msm8996-common/proprietary/vendor/lib64/libqcci_legacy.so:system/vendor/lib64/libqcci_legacy.so \
     vendor/xiaomi/msm8996-common/proprietary/vendor/lib64/libqcmaputils.so:system/vendor/lib64/libqcmaputils.so \
     vendor/xiaomi/msm8996-common/proprietary/vendor/lib64/libqdi.so:system/vendor/lib64/libqdi.so \
