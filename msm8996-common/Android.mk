327e8f4549ef7fc431b0377214c157aa4163e6a4
Bruno Martins <bgcngm@gmail.com>
2018-04-13 18:30:03 +0100

    msm8996-common: Update to MIUI 8.4.10 Oreo blobs
     * From: Xiaomi/gemini/gemini:8.0.0/OPR1.170623.032/8.4.10:user/release-keys
    
    Change-Id: I52e61e3297521eaca249c181e0a3d754f59443ed

--- msm8996-common/Android.mk
+++ msm8996-common/Android.mk
@@ -88,6 +88,17 @@ LOCAL_MODULE_SUFFIX := .apk
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := uceShimService
+LOCAL_MODULE_OWNER := xiaomi
+LOCAL_SRC_FILES := proprietary/app/uceShimService/uceShimService.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_DEX_PREOPT := false
+LOCAL_MODULE_SUFFIX := .apk
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := CNEService
 LOCAL_MODULE_OWNER := xiaomi
 LOCAL_SRC_FILES := proprietary/priv-app/CNEService/CNEService.apk
@@ -163,6 +174,16 @@ LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := JAVA_LIBRARIES
 LOCAL_MODULE_SUFFIX := .jar
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := ReferenceFeature
+LOCAL_MODULE_OWNER := xiaomi
+LOCAL_SRC_FILES := proprietary/vendor/etc/lm/ReferenceFeature.xml
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_SUFFIX := .xml
+LOCAL_VENDOR_MODULE := true
 include $(BUILD_PREBUILT)
 
 endif
