b5229ba4cf380b130372e34b9fc6cf94af7da411
ZeeLog <Danjel.Zammit@gmail.com>
2018-12-05 23:39:02 +0100

    msm8953-common: Update CNE & DPM blobs from Daisy
    * From xiaomi/daisy/daisy_sprout:9/PKQ1.180917.001/V10.0.1.0.PDLMIFJ:user/release-keys
    
    Change-Id: I7bc94ce9dc5158e77116ede4a6285b42456f9d4b

--- msm8953-common/Android.mk
+++ msm8953-common/Android.mk
@@ -101,18 +101,6 @@ LOCAL_PRIVILEGED_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := dpmserviceapp
-LOCAL_MODULE_OWNER := xiaomi
-LOCAL_SRC_FILES := proprietary/priv-app/dpmserviceapp/dpmserviceapp.apk
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := APPS
-LOCAL_DEX_PREOPT := false
-LOCAL_MODULE_SUFFIX := .apk
-LOCAL_PRIVILEGED_MODULE := true
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := ims
 LOCAL_MODULE_OWNER := xiaomi
 LOCAL_SRC_FILES := proprietary/priv-app/ims/ims.apk
