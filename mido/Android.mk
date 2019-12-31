809f0f0a98a6a4ab632c56a46ea58f39dc769348
Bruno Martins <bgcngm@gmail.com>
2019-12-31 23:22:05 +0100

    mido: Move offline charging daemon to new path
    Change-Id: Icf356c880b10f241e6394ece8136977d5ba20f6a

--- mido/Android.mk
+++ mido/Android.mk
@@ -18,14 +18,4 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),mido)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := chargeonlymode
-LOCAL_MODULE_OWNER := xiaomi
-LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT_SBIN)
-LOCAL_UNSTRIPPED_PATH := $(TARGET_ROOT_OUT_SBIN_UNSTRIPPED)
-LOCAL_SRC_FILES := proprietary/rootfs/sbin/chargeonlymode
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := EXECUTABLES
-include $(BUILD_PREBUILT)
-
 endif
