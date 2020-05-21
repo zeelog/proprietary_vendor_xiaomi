cb8d1907adc98750997e3c4fae1e9a88409d44e9
Albert I <kras@raphielgang.org>
2020-05-22 02:15:42 +0800

    mido: Switch to thermal blobs from olive V10.3.5.0.PCNCNXM
    * Xiaomi/olive/olive:9/PKQ1.190319.001/V10.3.5.0.PCNCNXM:user/release-keys
    * Required for thermal-engine to actually work with msm-4.9
    
    Signed-off-by: Albert I <kras@raphielgang.org>
    Change-Id: I4727522dc1c1044befafd7beea9e7d70579d4822

--- mido/mido-vendor.mk
+++ mido/mido-vendor.mk
@@ -1093,7 +1093,7 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/mido/proprietary/vendor/lib64/libslimclient.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libslimclient.so \
     vendor/xiaomi/mido/proprietary/vendor/lib64/libssd.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libssd.so \
     vendor/xiaomi/mido/proprietary/vendor/lib64/libsystem_health_mon.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libsystem_health_mon.so \
-    vendor/xiaomi/mido/proprietary/vendor/lib64/libthermalioctl.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libthermalioctl.so \
+    vendor/xiaomi/mido/proprietary/vendor/lib64/libthermalfeature.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libthermalfeature.so \
     vendor/xiaomi/mido/proprietary/vendor/lib64/libtime_genoff.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libtime_genoff.so \
     vendor/xiaomi/mido/proprietary/vendor/lib64/libtinyxml2_1.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libtinyxml2_1.so \
     vendor/xiaomi/mido/proprietary/vendor/lib64/libwms.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libwms.so \
